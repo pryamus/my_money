32cc5b9afc34104b673bd9de7e0cc4f0a402bdda
pryamus <9073321@mail.ru>
2025-10-31 12:44:20 +0300

    Add methods to class Money

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-class Money:
+class Money(object):
     def __init__(self, rub: int, kop: int):
         self.rub = rub + kop // 100
         self.kop = kop % 100 # копейки не должны превышать 99
 
+    def get_rub(self):
+        """ self - это ссылка на конкретный экземпляр при вызове метода. """
+        return self.rub
+
+    def get_kop(self):
+        return self.kop
+
+    def __str__(self) -> str:
+        return f'{self.rub} руб. {self.kop} коп.'
+
+    def __add__(self, other):
+        """ Реализация оператора +. Главные - это левый операнд.
+            Dunder(magic) метод вызывается неявно, когда объект участвует в
+            соответствующей операции.
+        """
+        return Money(self.rub + other.rub, self.kop + other.kop)
+
 
 if __name__ == '__main__':
     money1 = Money(20, 120)
-    money2 = Money(30, 330)
+    money2 = Money(30, 390)
     print(f'{money1.rub = }, {money1.kop = }')
     # Скопировать строку: Ctrl + D
     print(f'{money2.rub = }, {money2.kop = }')
+    # Явный вызов явных методов
+    print(money1.get_rub(), money1.get_kop())  # Money.get_rub(self=money1)
+    print(money1)
+    print(money2)
+    # Проверка работы оператора +
+    money3 = money1 + money2
+    print(money3)
+    print("Атрибуты экземпляра:", vars(money1)) # вернет словарь полей(fields)
